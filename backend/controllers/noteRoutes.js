8d9564f4f9395b773a4b5d5f9f97a5699e30cac1
YasmeenHammam <hammamyasmeen7@gmail.com>
2024-07-17 17:05:56 +0300

    intial work

---
+++ backend/controllers/noteRoutes.js
@@ -0,0 +1,153 @@
+const express = require("express");
+const noteRouter = express.Router();
+const Note = require("../models/note");
+
+const { request } = require("http");
+
+
+
+//GET 10 notes.
+noteRouter.get("/", async (request, response) => {
+  try {
+    const activePage = parseInt(request.query.activePage) || 1;
+    const postsPerPage = parseInt(request.query.postsPerPage) || 10;
+
+    const skip = (activePage - 1) * postsPerPage;
+
+    const count = await Note.countDocuments({});
+
+    const notes = await Note.find({})
+      .sort({ id: 1 })
+      .skip(skip)
+      .limit(postsPerPage);
+
+    response.status(200).json({ count, notes });
+  } catch (error) {
+    console.log(error);
+    response.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// POST new note
+noteRouter.post("/", async (request, response) => {
+  const { content } = request.body;
+
+  if (
+    content === undefined ||
+    content === null ||
+    typeof content !== "string"
+  ) {
+    return response
+      .status(400)
+      .json({ error: "Missing or Wrong fields in the request" });
+  }
+
+  const count = await Note.countDocuments({});
+  const customId = count + 1;
+
+  const note = new Note({
+    id: customId,
+    title: `Note ${customId}`,
+    author: {
+      name: `Author ${customId}`,
+      email: `mail_${customId}@gmail.com`,
+    },
+    content: content,
+  });
+  try {
+    const savedNote = await note.save();
+    response.status(201).json(savedNote);
+  } catch {
+    response.status(500).json({ error: "Cannot Save Note" });
+  }
+});
+
+// GET the i-th note.
+noteRouter.get("/:id", async (request, response) => {
+  const i = request.params.id;
+  try {
+    const notes = await Note.find({})
+      .sort({ id: 1 })
+      .skip(i - 1)
+      .limit(1);
+    const note = notes[0];
+
+    if (note) {
+      response.status(200).json(note);
+    } else {
+      response.status(404).json({ error: "Note not found" });
+    }
+  } catch {
+    response.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// DELETE the i-th Note.
+noteRouter.delete("/:id", async (request, response) => {
+  const i = request.params.id;
+
+  try {
+    const notes = await Note.find({})
+      .sort({ id: 1 })
+      .skip(i - 1)
+      .limit(1);
+    const note = notes[0];
+
+    if (note) {
+      const deletedNote = await Note.deleteOne({ _id: note._id });
+      if (deletedNote) {
+        response.status(204).json("Note deleted successfully");
+      } else {
+        response.status(500).json({ error: "Error deleting note" });
+      }
+    } else {
+      response.status(404).json({ error: "Note not found" });
+    }
+  } catch {
+    response.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+//EDIT the i-th Note.
+noteRouter.put("/:id", async (request, response) => {
+  const i = request.params.id;
+  const newContent = request.body.content;
+
+  if (
+    newContent === undefined ||
+    newContent === null ||
+    typeof newContent !== "string"
+  ) {
+    return response
+      .status(400)
+      .json({ error: "Missing or Wrong fields in the request" });
+  }
+
+  try {
+    const notes = await Note.find({})
+      .sort({ id: 1 })
+      .skip(i - 1)
+      .limit(1);
+    const note = notes[0];
+
+    if (note) {
+      const updatedNote = await Note.updateOne(
+        { _id: note._id },
+        { content: newContent }
+      );
+
+      if (updatedNote) {
+        response.status(200).json(updatedNote);
+      } else {
+        response.status(500).json({ error: "Error updating note" });
+      }
+    } else {
+      response.status(404).json({ error: "Note not found" });
+    }
+  } catch {
+    response.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+
+module.exports = noteRouter;
--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,10 @@ const app = express();
 
 require("dotenv").config();
 
-const Note = require("./models/note");
-
 app.use(express.static("dist"));
 
 const fs = require("fs");
 const path = require("path");
-
 const requestLogger = (request, response, next) => {
   const filepath = path.join(__dirname, "log.txt");
 
@@ -26,159 +23,16 @@ const requestLogger = (request, response, next) => {
 };
 
 const cors = require("cors");
-const { request } = require("http");
-
-app.use(cors());
-
-app.use(express.json());
-app.use(requestLogger);
-
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
+app.use(cors());
 
-//GET 10 notes.
-app.get("/notes", async (request, response) => {
-  try {
-    const activePage = parseInt(request.query.activePage) || 1;
-    const postsPerPage = parseInt(request.query.postsPerPage) || 10;
+const notesRouter = require("./controllers/noteRoutes");
 
-    const skip = (activePage - 1) * postsPerPage;
-
-    const count = await Note.countDocuments({});
-
-    const notes = await Note.find({})
-      .sort({ id: 1 })
-      .skip(skip)
-      .limit(postsPerPage);
-
-    response.status(200).json({ count, notes });
-  } catch (error) {
-    console.log(error);
-    response.status(500).json({ error: "Internal Server Error" });
-  }
-});
-
-// POST new note
-app.post("/notes", async (request, response) => {
-  const { content } = request.body;
-
-  if (
-    content === undefined ||
-    content === null ||
-    typeof content !== "string"
-  ) {
-    return response
-      .status(400)
-      .json({ error: "Missing or Wrong fields in the request" });
-  }
-
-  const count = await Note.countDocuments({});
-  const customId = count + 1;
-
-  const note = new Note({
-    id: customId,
-    title: `Note ${customId}`,
-    author: {
-      name: `Author ${customId}`,
-      email: `mail_${customId}@gmail.com`,
-    },
-    content: content,
-  });
-  try {
-    const savedNote = await note.save();
-    response.status(201).json(savedNote);
-  } catch {
-    response.status(500).json({ error: "Cannot Save Note" });
-  }
-});
-
-// GET the i-th note.
-app.get("/notes/:id", async (request, response) => {
-  const i = request.params.id;
-  try {
-    const notes = await Note.find({})
-      .sort({ id: 1 })
-      .skip(i - 1)
-      .limit(1);
-    const note = notes[0];
-
-    if (note) {
-      response.status(200).json(note);
-    } else {
-      response.status(404).json({ error: "Note not found" });
-    }
-  } catch {
-    response.status(500).json({ error: "Internal Server Error" });
-  }
-});
-
-// DELETE the i-th Note.
-app.delete("/notes/:id", async (request, response) => {
-  const i = request.params.id;
-
-  try {
-    const notes = await Note.find({})
-      .sort({ id: 1 })
-      .skip(i - 1)
-      .limit(1);
-    const note = notes[0];
-
-    if (note) {
-      const deletedNote = await Note.deleteOne({ _id: note._id });
-      if (deletedNote) {
-        response.status(204).json("Note deleted successfully");
-      } else {
-        response.status(500).json({ error: "Error deleting note" });
-      }
-    } else {
-      response.status(404).json({ error: "Note not found" });
-    }
-  } catch {
-    response.status(500).json({ error: "Internal Server Error" });
-  }
-});
-
-//EDIT the i-th Note.
-app.put("/notes/:id", async (request, response) => {
-  const i = request.params.id;
-  const newContent = request.body.content;
-
-  if (
-    newContent === undefined ||
-    newContent === null ||
-    typeof newContent !== "string"
-  ) {
-    return response
-      .status(400)
-      .json({ error: "Missing or Wrong fields in the request" });
-  }
-
-  try {
-    const notes = await Note.find({})
-      .sort({ id: 1 })
-      .skip(i - 1)
-      .limit(1);
-    const note = notes[0];
-
-    if (note) {
-      const updatedNote = await Note.updateOne(
-        { _id: note._id },
-        { content: newContent }
-      );
-
-      if (updatedNote) {
-        response.status(200).json(updatedNote);
-      } else {
-        response.status(500).json({ error: "Error updating note" });
-      }
-    } else {
-      response.status(404).json({ error: "Note not found" });
-    }
-  } catch {
-    response.status(500).json({ error: "Internal Server Error" });
-  }
-});
+app.use(express.json());
+app.use(requestLogger);
+app.use("/notes", notesRouter);
 
 app.use(unknownEndpoint);
 
